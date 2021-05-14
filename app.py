b29761a76fc41ab01792934bf0f75c4141fbabb1
omprakashselvaraj <69300839+omprakashselvaraj@users.noreply.github.com>
2021-05-14 20:17:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask,render_template,request,send_file
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import matplotlib.pyplot as plt
+#from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvaimport matplotlib.pyplot as plt
 import numpy as np
-from io import BytesIO
-import jinja2
-plt.style.use('ggplot')
-
-msg=[]
+from numpy import pi
+import pandas as pd
+import pygal
     
 
 
@@ -19,11 +16,11 @@ def main():
 
 @app.route('/input',methods=['GET','POST'])
 def input():
+    msg=[]
     if request.method=="POST":
         detail=request.form
         builtup=int(detail['builtup'])
         cost=int(detail['cost'])
-        print(builtup,cost)
         msg.append(builtup)
         msg.append(cost)
         tot=cost*builtup
@@ -41,21 +38,49 @@ def input():
         msg.append(0.18*builtup)
         msg.append(8*builtup)
         msg.append(1.3*builtup)
+        msg.append(0.27*tot)
+        msg.append(0.184*tot)
+        msg.append(0.111*tot)
+        msg.append(0.169*tot)
+        msg.append(0.178*tot)
+        msg.append(0.139*tot)
 
-        return render_template('output.html', msg=msg)
 
-@app.route('/plotcall')
-def plotcall():
-    x=np.array(msg[3:9])
-    mylabels=['cement(16.4%)','sand(12.3%)','aggregate(7.4%)','steel(24.6%)','finitures(16.5%)','fittings(22.8%']
-    plt.title('Approx. cost on various work of materials')
-    plt.pie(x,labels=mylabels)
-    figfile = BytesIO()
-    plt.savefig(figfile, format='png')
-    figfile.seek(0)  # rewind to beginning of file
-    import base64
-    figdata_png = base64.b64encode(figfile.getvalue())
-    return figdata_png
+
+
+
+        results = [
+    (msg[3],'Cement'),
+    (msg[4],'Sand'),
+    (msg[5],'Aggregate'),
+    (msg[6],'Steel'),
+    (msg[7],'Finishers'),
+    (msg[8],'Fittings'),
+ 
+    ]
+
+        pie_chart = pygal.Pie(height=500)
+        pie_chart.title = 'Approx. cost on various work of materials'
+        for r in results:
+            pie_chart.add(r[1], r[0])
+        pie_chart.value_formatter = lambda x: "%.15f" % x
+        piech=pie_chart.render_data_uri()
+    
+
+        bar_chart = pygal.Bar(height=500)  # instance of Bar class
+        bar_chart.title = 'Construction cost Splitup for 6 months'  # title of bar chart
+        bar_chart.add('cost', [msg[16],msg[17],msg[18],msg[19],msg[20],msg[21]])  # add fibo data to chart
+        bar_chart.x_labels = 'Month 1','Month 2','Month 3','Month 4','Month 5','Month 6'
+        chart = bar_chart.render_data_uri()  
+
+      
+        html = render_template(
+        'output.html',
+        msg=msg,
+        piech=piech,
+        chart=chart   
+    )
+    return (html)
 
 
 if __name__=='__main__':
