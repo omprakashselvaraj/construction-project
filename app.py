574be0509fbe7686061e473fd1466d61303bb45b
omprakashselvaraj <69300839+omprakashselvaraj@users.noreply.github.com>
2021-05-19 19:19:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,61 +17,62 @@ def main():
 @app.route('/input',methods=['GET','POST'])
 def input():
     msg=[]
-    detail=request.form
-    builtup=int(detail['builtup'])
-    cost=int(detail['cost'])
-    msg.append(builtup)
-    msg.append(cost)
-    tot=cost*builtup
-    msg.append(tot)
-    msg.append(0.164*tot)
-    msg.append(0.123*tot)
-    msg.append(0.0742*tot)
-    msg.append(0.246*tot)
-    msg.append(0.165*tot)
-    msg.append(0.228*tot)
-    msg.append(0.4*builtup)
-    msg.append(0.816*builtup)
-    msg.append(0.608*builtup)
-    msg.append(4*builtup)
-    msg.append(0.18*builtup)
-    msg.append(8*builtup)
-    msg.append(1.3*builtup)
-    msg.append(0.27*tot)
-    msg.append(0.184*tot)
-    msg.append(0.111*tot)
-    msg.append(0.169*tot)
-    msg.append(0.178*tot)
-    msg.append(0.139*tot)
+    if methods=='POST':
+        detail=request.form
+        builtup=int(detail['builtup'])
+        cost=int(detail['cost'])
+        msg.append(builtup)
+        msg.append(cost)
+        tot=cost*builtup
+        msg.append(tot)
+        msg.append(0.164*tot)
+        msg.append(0.123*tot)
+        msg.append(0.0742*tot)
+        msg.append(0.246*tot)
+        msg.append(0.165*tot)
+        msg.append(0.228*tot)
+        msg.append(0.4*builtup)
+        msg.append(0.816*builtup)
+        msg.append(0.608*builtup)
+        msg.append(4*builtup)
+        msg.append(0.18*builtup)
+        msg.append(8*builtup)
+        msg.append(1.3*builtup)
+        msg.append(0.27*tot)
+        msg.append(0.184*tot)
+        msg.append(0.111*tot)
+        msg.append(0.169*tot)
+        msg.append(0.178*tot)
+        msg.append(0.139*tot)
 
 
 
 
 
-    results = [(msg[3],'Cement'),(msg[4],'Sand'),(msg[5],'Aggregate'),(msg[6],'Steel'),(msg[7],'Finishers'),(msg[8],'Fittings')]
+        results = [(msg[3],'Cement'),(msg[4],'Sand'),(msg[5],'Aggregate'),(msg[6],'Steel'),(msg[7],'Finishers'),(msg[8],'Fittings')]
 
-    pie_chart = pygal.Pie(height=500)
-    pie_chart.title = 'Approx. cost on various work of materials'
-    for r in results:
-        pie_chart.add(r[1], r[0])
-    pie_chart.value_formatter = lambda x: "%.15f" % x
-    piech=pie_chart.render_data_uri()
+        pie_chart = pygal.Pie(height=500)
+        pie_chart.title = 'Approx. cost on various work of materials'
+        for r in results:
+            pie_chart.add(r[1], r[0])
+        pie_chart.value_formatter = lambda x: "%.15f" % x
+        piech=pie_chart.render_data_uri()
 
 
-    bar_chart = pygal.Bar(height=500)  # instance of Bar class
-    bar_chart.title = 'Construction cost Splitup for 6 months'  # title of bar chart
-    bar_chart.add('cost', [msg[16],msg[17],msg[18],msg[19],msg[20],msg[21]])  # add fibo data to chart
-    bar_chart.x_labels = 'Month 1','Month 2','Month 3','Month 4','Month 5','Month 6'
-    chart = bar_chart.render_data_uri()  
+        bar_chart = pygal.Bar(height=500)  # instance of Bar class
+        bar_chart.title = 'Construction cost Splitup for 6 months'  # title of bar chart
+        bar_chart.add('cost', [msg[16],msg[17],msg[18],msg[19],msg[20],msg[21]])  # add fibo data to chart
+        bar_chart.x_labels = 'Month 1','Month 2','Month 3','Month 4','Month 5','Month 6'
+        chart = bar_chart.render_data_uri()  
 
-    
-    html = render_template(
-    'output.html',
-    msg=msg,
-    piech=piech,
-    chart=chart   
-    )
-    return (html)
+
+        html = render_template(
+        'output.html',
+        msg=msg,
+        piech=piech,
+        chart=chart   
+        )
+        return (html)
 
 
 if __name__=='__main__':
